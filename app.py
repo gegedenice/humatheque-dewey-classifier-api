82edb7efe06191f2d03242dc538f0f1dc230121f
Géraldine Geoffroy <grldn.geoffroy@gmail.com>
2026-06-02 18:57:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -326,5 +326,4 @@ async def classify_endpoint(
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=8002)
